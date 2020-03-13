4e869a6c7619654a65cb9dfd5ad02d98e6578355
Rust Saiargaliev <fly.amureki@gmail.com>
2020-03-13 09:13:56 +0100

    Parser: make a proper POST request (#6)

--- data/parser.py
+++ data/parser.py
@@ -1,18 +1,26 @@
 import httpx
 
-STATS_URL_TEMPLATE = "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=1=1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&cacheHint=true&outStatistics={out_statistics}"
-STATS_PER_REGION_URL = "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/2/query?f=json&where=Confirmed > 0&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Confirmed desc&resultOffset=0&cacheHint=true"
+ARCGIS_BASE_URL = "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/ArcGIS/rest/services/ncov_cases/FeatureServer"
+
+ARCGIS_API_URL = f"{ARCGIS_BASE_URL}/1/query"
+ARCGIS_REGION_API_URL = f"{ARCGIS_BASE_URL}/2/query"
 
 
 def parse_summary_data():
     stats = {}
     for stat in ["confirmed", "deaths", "recovered"]:
-        out_statistics = (
-            '[{"statisticType":"sum","onStatisticField":"%s","outStatisticFieldName":"value"}]'
-            % stat
+        response = httpx.post(
+            ARCGIS_API_URL,
+            data={
+                "where": "1=1",
+                "outFields": "*",
+                "cacheHint": "true",
+                "outStatistics": '[{"statisticType":"sum","onStatisticField":"%s","outStatisticFieldName":"value"}]'
+                % stat,
+                "f": "pjson",
+            },
+            timeout=25.0,
         )
-        url = STATS_URL_TEMPLATE.format(out_statistics=out_statistics)
-        response = httpx.post(url, timeout=25.0)
         response.raise_for_status()
         raw_data = response.json()
         value = raw_data["features"][0]["attributes"]["value"]
@@ -21,7 +29,17 @@ def parse_summary_data():
 
 
 def parse_per_region_data():
-    response = httpx.post(STATS_PER_REGION_URL, timeout=25.0)
+    response = httpx.post(
+        ARCGIS_REGION_API_URL,
+        data={
+            "where": "Confirmed>0",
+            "orderByFields": "Confirmed desc",
+            "outFields": "*",
+            "cacheHint": "true",
+            "f": "pjson",
+        },
+        timeout=25.0,
+    )
     response.raise_for_status()
     raw_data = response.json()
     regions_raw_data = raw_data["features"]
