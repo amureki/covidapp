3461b73ec5417bec7d2c1f0fea31822fdcca8bbe
Rust Saiargaliev <rustem.sayargaliev@thermondo.de>
2020-03-21 12:14:17 +0100

    Clean up data a bit more

--- data/management/commands/clean_region_data.py
+++ data/management/commands/clean_region_data.py
@@ -57,18 +57,22 @@ class Command(BaseCommand):
     help = "Clean region naming data."
 
     def handle(self, *args, **options):
+        regions = []
         for summary in Summary.objects.all():
             to_update = False
             raw_orig = summary.raw_data
             for item in raw_orig:
                 region = item["attributes"]["Country_Region"]
+                if region not in regions:
+                    regions.append(region)
+
                 if region == "Mainland China":
                     item["attributes"]["Country_Region"] = "China"
                     to_update = True
                 if region == "Iran (Islamic Republic of)":
                     item["attributes"]["Country_Region"] = "Iran"
                     to_update = True
-                if region in ("South Korea", "Republic of Korea"):
+                if region in ("South Korea", "Republic of Korea", '"Korea; South"'):
                     item["attributes"]["Country_Region"] = "Korea, South"
                     to_update = True
                 if region == "UK":
@@ -80,7 +84,10 @@ class Command(BaseCommand):
                 if region == "Czech Republic":
                     item["attributes"]["Country_Region"] = "Czechia"
                     to_update = True
-                if region == "Taiwan":
+                if region == "Republic of Moldova":
+                    item["attributes"]["Country_Region"] = "Moldova"
+                    to_update = True
+                if region in ("Taiwan", "Taipei and environs"):
                     item["attributes"]["Country_Region"] = "Taiwan*"
                     to_update = True
                 if region == "Russian Federation":
@@ -89,7 +96,15 @@ class Command(BaseCommand):
                 if region == "Viet Nam":
                     item["attributes"]["Country_Region"] = "Vietnam"
                     to_update = True
+                if region == "Vatican City":
+                    item["attributes"]["Country_Region"] = "Holy See"
+                    to_update = True
+                if region in ("Gambia", "The Gambia"):
+                    item["attributes"]["Country_Region"] = "Gambia, The"
+                    to_update = True
+
             if to_update:
                 summary.raw_data = raw_orig
                 regions_data = summary.set_regions_data()
                 Summary.objects.filter(pk=summary.pk).update(regions_data=regions_data)
+        print(sorted(list(filter(None, regions))))
