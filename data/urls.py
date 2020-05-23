4691e413f6e798a9e997a8140074e9070ccdd0cd
Rust Saiargaliev <rustem.sayargaliev@thermondo.de>
2020-05-23 12:50:54 +0200

    Add support page

--- data/urls.py
+++ data/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from data.views import IndexPageView, RegionsListView, RegionDetailView
 
@@ -6,4 +7,7 @@ urlpatterns = [
     path("", IndexPageView.as_view(), name="index"),
     path("regions/", RegionsListView.as_view(), name="regions"),
     path("region/<slug:region>/", RegionDetailView.as_view(), name="region"),
+    path(
+        "support/", TemplateView.as_view(template_name="support.html"), name="support"
+    ),
 ]
