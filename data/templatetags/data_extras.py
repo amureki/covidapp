a75b5a57ee8bbb271d70943e98b490b9545bb5b9
Rust Saiargaliev <rustem.sayargaliev@thermondo.de>
2020-06-06 18:00:42 +0200

    Hotfix: handle no data
    Data source is now missing certain data, for example amout of recovered cases in Brazil.
    Quick fix to handle this temporary issue.

--- data/templatetags/data_extras.py
+++ data/templatetags/data_extras.py
@@ -5,6 +5,9 @@ register = template.Library()
 
 @register.filter
 def as_percentage_of(part, whole):
+    part = part or 0
+    whole = whole or 0
+
     try:
         percentage = round(float(part) / whole * 100, 1)
         if percentage % 1 == 0:
